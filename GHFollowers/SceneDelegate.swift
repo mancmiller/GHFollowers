013031a312cf9e7ed6de076d4521433fba03fc1b
mancmiller <manc.miller@gmail.com>
2023-03-27 13:00:48 +0600

    Added 3 subviews to the searchVC

--- GHFollowers/SceneDelegate.swift
+++ GHFollowers/SceneDelegate.swift
@@ -41,7 +41,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func createTabBar() -> UITabBarController {
         let tabBar = UITabBarController()
         UITabBar.appearance().tintColor = .systemGreen
-        UITabBar.appearance().backgroundColor = .tertiarySystemBackground
+        UITabBar.appearance().backgroundColor = .systemBackground
         tabBar.viewControllers = [createSearchNC(), createFavoritesNC()]
         
         return tabBar
