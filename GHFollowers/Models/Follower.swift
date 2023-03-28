a27362174f4c9f7ac87de94e31b3a859e2cf664e
mancmiller <manc.miller@gmail.com>
2023-03-28 11:56:37 +0600

    Created a Singleton

---
+++ GHFollowers/Models/Follower.swift
@@ -0,0 +1,14 @@
+//
+// Follower.swift
+//  GHFollowers
+//
+//  Created by Muslim on 27.03.2023.
+//
+
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
