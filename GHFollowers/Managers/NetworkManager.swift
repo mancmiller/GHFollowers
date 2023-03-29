3a6d855382bdc8037e3d1617aa266df6522e5be2
mancmiller <manc.miller@gmail.com>
2023-03-29 16:15:56 +0600

    Got network all for UserInfo working

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -28,7 +28,7 @@ class NetworkManager {
             }
             
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                    completed(.failure(.unableToComplete))
+                    completed(.failure(.invalidResponse))
                 return
             }
             
@@ -42,6 +42,41 @@ class NetworkManager {
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let followers = try decoder.decode([Follower].self, from: data)
                 completed(.success(followers))
+            } catch {
+                    completed(.failure(.invalidData))
+            }
+        }
+        task.resume()
+    }
+    
+    func getUserInfo(for username: String, completed: @escaping(Result<User, GFError>) -> Void){
+        let endpoint = baseURL + "\(username)"
+        guard let url = URL(string: endpoint) else {
+            completed(.failure(.invalidUsername))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                completed(.failure(.unableToComplete))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                    completed(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let user = try decoder.decode(User.self, from: data)
+                completed(.success(user))
             } catch {
                     completed(.failure(.invalidData))
             }
