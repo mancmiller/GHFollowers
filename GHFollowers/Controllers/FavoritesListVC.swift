9bf3c19c9997a3193cf22f0ec2476ddf002c3481
mancmiller <manc.miller@gmail.com>
2023-03-31 17:02:00 +0600

    Set up a PersistenceManager

--- GHFollowers/Controllers/FavoritesListVC.swift
+++ GHFollowers/Controllers/FavoritesListVC.swift
@@ -10,6 +10,14 @@ import UIKit
 class FavoritesListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink      
+        view.backgroundColor = .systemPink
+        PeristenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case .failure(let error):
+                break
+            }
+        }
     }
 }
